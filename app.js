f48d19550f61463a2fc15d62be61be3d2fa634f6
Mateus Costa <mateus.costa@roit.com.br>
2021-12-27 21:55:32 -0300

    feat: lifecycle hooks

--- app.js
+++ app.js
@@ -3,6 +3,30 @@ let vm = Vue.createApp({
     return {
       message: "Hello world!"
     }
+  },
+  beforeCreate() {
+    console.log('beforeCreate', this.message)
+  },
+  created() {
+    console.log('created', this.message)
+  },
+  beforeMount() {
+    console.log('beforeMount', this.$el)
+  },
+  mounted() {
+    console.log('mounted', this.$el)
+  },
+  beforeUpdate() {
+    console.log('beforeUpdated() function called!')
+  },
+  updated() {
+    console.log('updated() function called!')
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount() function called!')
+  },
+  unmounted() {
+    console.log('unmounted() function called!')
   }
 })
 
